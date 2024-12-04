74366065ad8b7617647e563f7c382d52d385ee3f
KiSa <danielmendes004@gmail.com>
2024-12-04 01:55:46 +0000

    ??

---
+++ Common/.cxx/Debug/2g463a81/x86/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := g
+LOCAL_SRC_FILES := C:\Users\da\Downloads\Juggluco-primary\Common\build\intermediates\cxx\Debug\2g463a81\obj\x86\libg.so
+LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_EXPORT_LDLIBS := -lc++_static -lc -llog -lGLESv3 -landroid -lEGL -lm -latomic -lz -ldl
+include $(PREBUILT_SHARED_LIBRARY)
+
